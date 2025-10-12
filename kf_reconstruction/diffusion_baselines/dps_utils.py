1c8e78cf488af61bfd44ec9e8a374712585b68d5
Sifan Wang <sifanw@sas.upenn.edu>
2025-10-12 10:24:54 -0400

    add baselines

--- kf_reconstruction/diffusion_baselines/dps_utils.py
+++ kf_reconstruction/diffusion_baselines/dps_utils.py
@@ -12,9 +12,13 @@ def get_div_res(uv, x0=0.0, x1=1.0, y0=0.0, y1=1.0):
     dx = (x1 - x0) / (nx - 1)
     dy = (y1 - y0) / (nt - 1)
 
-    u_x = jnp.gradient(u, dx, axis=2)
-    v_y = jnp.gradient(v, dy, axis=1)
+    u_x = (u[2:, 1:-1] - u[:-2, 1:-1]) / (2 * dx)
+    v_y = (v[1:-1, 2:] - v[1:-1, :-2]) / (2 * dy)
 
     res = u_x + v_y
+
+    # padding to keep same shape as uv
+    res = jnp.pad(res, ((1, 1), (1, 1), (0, 0), (0, 0)), mode='edge')
+
     return res
 
